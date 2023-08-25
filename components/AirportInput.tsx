d21ee927ac6518c7e7808dbf68729765cdf2717c
mscankat <m.salihcankat@gmail.com>
2023-08-25 18:32:01 +0300

    fix:clear suggestions

--- components/AirportInput.tsx
+++ components/AirportInput.tsx
@@ -6,30 +6,41 @@ export default function AirportInput() {
   const [suggestions, setSuggestions] = useState([]);
   const handleChange = async (e: any) => {
     setInput(e.target.value);
-    const reqBody = { query: e.target.value };
-    const response = await fetch("http://localhost:3000/api", {
-      method: "POST",
-      body: JSON.stringify(reqBody),
-    });
-    const data = await response.json();
-    console.log(data);
-    setSuggestions(data);
+    if (e.target.value !== "") {
+      const reqBody = { query: e.target.value };
+      const response = await fetch("http://localhost:3000/api", {
+        method: "POST",
+        body: JSON.stringify(reqBody),
+      });
+      const data = await response.json();
+      console.log(data);
+      setSuggestions(data);
+    } else {
+      setSuggestions([]);
+    }
   };
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col relative">
       <input
         type="text"
         onChange={handleChange}
         value={input}
-        className="relative w-96 bg-slate-300 p-3 rounded-lg"
+        className=" w-96 bg-slate-300 p-3 rounded-lg"
         placeholder="from"
       />
-      <ul className="absolute -bottom-16 bg-zinc-100 p-4 rounded-lg ">
-        <li className="text-right">X</li>
-        {suggestions.slice(0, 10).map((suggestion, index) => {
-          return <li key={index}> {suggestion.code + suggestion.city}</li>;
-        })}
-      </ul>
+      {suggestions[0] && (
+        <ul className="absolute top-14 bg-zinc-100 p-4 rounded-lg ">
+          <li className="text-right">X</li>
+          {suggestions.slice(0, 10).map((suggestion, index) => {
+            return (
+              <li className="mt-1" key={index}>
+                {" "}
+                {suggestion.code + " " + suggestion.city}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
