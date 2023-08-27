4078e00ca3c28f74acfa36c8439f5ad55a257cd5
mscankat <m.salihcankat@gmail.com>
2023-08-27 14:22:15 +0300

    fix:cors

--- app/api/flights/route.ts
+++ app/api/flights/route.ts
@@ -37,7 +37,10 @@ export async function POST(request: Request) {
     }
   }
 
-  return NextResponse.json({
-    flights: flightResults,
-  });
+  return NextResponse.json(
+    {
+      flights: flightResults,
+    },
+    { status: 200, headers: { "Access-Control-Allow-Origin": "*" } }
+  );
 }
--- app/api/route.ts
+++ app/api/route.ts
@@ -22,5 +22,8 @@ export async function POST(request: Request) {
     }
   }
 
-  return NextResponse.json(result);
+  return NextResponse.json(result, {
+    status: 200,
+    headers: { "Access-Control-Allow-Origin": "*" },
+  });
 }
